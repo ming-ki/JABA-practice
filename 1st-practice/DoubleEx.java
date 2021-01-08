d5834f2a2b718d6244c3301c38418beed3c12158
ming-ki <wbakswl010@naver.com>
2021-01-08 10:18:10 +0900

    commit

---
+++ 1st-practice/DoubleEx.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class DoubleEx {
+		int abc;
+	public static void main(String[] args)
+	{
+		var dNum = 3.14;
+		float fNum = 3.14F;
+		
+		System.out.println(dNum);
+		
+		var num = 10;
+		System.out.println(num);
+		
+		
+		
+	}
+}
